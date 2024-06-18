19cef9d8381bc2ddadae3df28dec297e97671a03
Khairul2991 <auni3934@gmail.com>
2024-06-18 22:37:16 +0700

    fix dependencies not found

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation(libs.play.services.auth)
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
-    implementation (libs.google.googleid)
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
@@ -71,6 +70,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
     implementation ("com.github.qamarelsafadi:CurvedBottomNavigation:0.1.3")
 }
